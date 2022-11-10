e17bc1bd6a0370ce05d0fc84e3b963e4df784f66
B0501 <113827746+B0501@users.noreply.github.com>
2022-11-10 13:24:32 +0530

    Delete Project Development Phase/Sprint 2 directory

--- Project Development Phase/Sprint 2/sqlite_db_setup.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('student_database.db')
-print("Opened database successfully")
-
-conn.execute('CREATE TABLE students ( username TEXT, email TEXT, password TEXT )')
-print("Table created successfully")
-conn.close()
